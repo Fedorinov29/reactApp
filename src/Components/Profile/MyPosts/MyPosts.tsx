78e36f96eada91d9ef64231a1546fce1e60a801a
Oleksii <a.fedorinov1993@gmail.comm>
2022-05-08 23:35:38 +0300

    Add props to Post

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import s from "./Post.module.css";
+import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
   return (
     <div>
       My Postttt
-      <div>New Post</div>
+      <div className={s.addPost}>
+        <textarea></textarea>
+        <button>Add new Post</button>
+      </div>
       <div>
-        <Post />
-        <Post />
-        <Post />
+        <Post message="Hi , its my first post on this page" />
+        <Post message="And now i will show you ..." />
+        <Post message="russian warship go fuck yourself" />
       </div>
     </div>
   );
--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props: any) => {
   return (
     <div className={s.item}>
       <img
         src="https://assets.gq.ru/photos/5d9f45713a54c0000840d54d/16:9/w_2560%2Cc_limit/0.jpg"
         alt=""
-      />{" "}
-      Post 1
+      />
+      <p>{props.message}</p>
+      <div>
+        <span>Like</span>
+      </div>
     </div>
   );
 };
