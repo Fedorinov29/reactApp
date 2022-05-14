2bebc686727550f004bd347355a35b9962b63450
Oleksii <a.fedorinov1993@gmail.comm>
2022-05-14 23:13:12 +0300

    create components : DialogItem and Messages

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,19 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
 import s from "./Dialogs.module.css";
-
-const DialogItem = (props: any) => {
-  let path = "/dialogs/" + props.id;
-  return (
-    <div className={s.dialog + " " + s.active}>
-      <NavLink to={path}>{props.name}</NavLink>{" "}
-    </div>
-  );
-};
-
-const MessageItem = (props: any) => {
-  return <div className={s.message}>{props.message}</div>;
-};
+import MessageItem from "./Message/Message";
 
 const Dialogs = () => {
   let dialogData = [
